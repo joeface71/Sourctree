0820071cb69fc6f664a60aaa402f9ba12926b127
joeface71 <joefacenda@yahoo.com>
2022-03-17 18:26:45 -0400

    delegate tutorial

---
+++ Assets/Testing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Testing : MonoBehaviour
+{
+    public delegate void TestDelegate();
+    public delegate bool TestBoolDelegate(int i);
+    
+    private TestDelegate testDelegateFunction;
+    private TestBoolDelegate testBoolDelegate;
+
+    private void Start() {
+        testBoolDelegate = MyTestBoolDelegateFunction;
+
+        Debug.Log(testBoolDelegate(6));
+    }
+
+    private void MyTestDelegateFunction()
+    {
+        Debug.Log("MyTestDelegateFunction");
+    }
+
+    private void MySecondDelegateFunction()
+    {
+        Debug.Log("MySecondDelegateFunction");
+    }
+
+    private bool MyTestBoolDelegateFunction(int i)
+    {
+        return i > 5;
+    }
+    
+}
